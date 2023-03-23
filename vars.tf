bfd4d2f676fd4721b407efaa553d2b26b37127e4
mettashalini89@gmail.com <CatMatDog@123>
2023-03-23 16:38:50 -0600

    alb config using aws

--- vars.tf
+++ vars.tf
@@ -1,8 +1,8 @@
 variable "env" {}
-variable "subnet_ids" {}
+
 variable "tags" {}
 variable "name" {}
-variable "subnet_name" {}
+
 variable "internal" {}
 variable "load_balancer_type" {}
 variable "enable_deletion_protection" {
